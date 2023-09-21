18e40025c1dc7836f1dcd6ebe5cd9736ff78b6c5
korolevainna <innkoroleva2017@gmail.com>
2023-09-21 20:18:44 +0300

    auth2

--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 const {User} = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  role: 1,
+})
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -40,6 +46,42 @@ router.get('/signup', function (req, res) {
     },
   })
   // ↑↑ сюди вводимо JSON дані
+})
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: 'Помилка. Обов`язкові поля відсутні',
+    })
+  }
+
+  try {
+    const user = User.getByEmail(email)
+    if (user) {
+      return res.status(400).json({
+        message: 'Такий користувач вже існує',
+      })
+    }
+
+    const newUser = User.create({ email, password, role })
+
+    const session = session.create(newUser)
+
+    Confirm.create(newUser.email)
+
+    return res.status(200).json({
+      message: 'Користувач успішно зареєстрований',
+      session,
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Помилка створення користувача',
+    })
+  }
 })
 
 // Підключаємо роутер до бек-енду
